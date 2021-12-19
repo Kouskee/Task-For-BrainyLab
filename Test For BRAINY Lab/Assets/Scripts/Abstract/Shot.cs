f5783b101f7d63b40905ea0dcfcb1c42de81a124
Kouskee <62687243+Kouskee@users.noreply.github.com>
2021-12-19 15:19:59 +0700

    Dunno

--- Test For BRAINY Lab/Assets/Scripts/Abstract/Shot.cs
+++ Test For BRAINY Lab/Assets/Scripts/Abstract/Shot.cs
@@ -1,12 +1,11 @@
-
 using UnityEngine;
-
 public abstract class Shot : MonoBehaviour
 {
     protected virtual void Shooting(Transform _gun, GameObject _prefabBullet)
     {
         GameObject newBullet = Instantiate(_prefabBullet, _gun.transform.position, _gun.transform.localRotation);
-        newBullet.transform.Translate(_gun.transform.forward * 2f);
+        newBullet.transform.Translate(_gun.transform.forward);
+        newBullet.transform.forward = _gun.transform.forward;
         newBullet.GetComponent<Rigidbody>().velocity = _gun.transform.forward * 30f;
     }
 }
